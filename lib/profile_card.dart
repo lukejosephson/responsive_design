859d2a0682ed6c87cf16c27cd97703c1cc358add
lukejosephson <lukerjosephson@gmail.com>
2026-02-04 09:00:50 -0500

    updated a lil

--- lib/profile_card.dart
+++ lib/profile_card.dart
@@ -10,11 +10,30 @@ class ProfileCard extends StatelessWidget {
       body: Center(
         child: Container(
           color: Colors.grey[200],
-          child: Column(
-            children: [
-              _buildAvatar(),
-              Text('Poindexter Dankworth'),
-            ],
+          padding: const EdgeInsets.all(20),
+          constraints: const BoxConstraints(maxWidth:800),
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              if (constraints.maxWidth > 600) {
+                return Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    _buildAvatar(),
+                    SizedBox(width: 20),
+                    Expanded(child: _buildContent()),
+                  ],
+                );
+              } else {
+                return Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    _buildAvatar(),
+                    SizedBox(height: 15),
+                    _buildContent(),
+                  ],
+                );
+              }
+            },
           ),
         ),
       ),
@@ -28,10 +47,26 @@ Widget _buildAvatar() {
   return Container(
     width: 100,
     height: 100,
-    decoration: BoxDecoration(
-      color: Colors.blueAccent,
-      shape: BoxShape.circle,
-    ),
+    decoration: BoxDecoration(color: Colors.blueAccent, shape: BoxShape.circle),
     child: Icon(Icons.person, size: 75, color: Colors.white),
+  );
+}
+
+// content widget for the profile
+Widget _buildContent() {
+  return Column(
+    mainAxisSize: MainAxisSize.min,
+    crossAxisAlignment:
+        CrossAxisAlignment.start, // .start left justifies the column
+    children: [
+      Text(
+        'Hingle McCringleberry',
+        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      ),
+      Text('Major: Computer Science'),
+      Text('Favorite Class: CS364'),
+      SizedBox(height: 20),
+      ElevatedButton(onPressed: () {}, child: Text('Log in')),
+    ],
   );
 }
